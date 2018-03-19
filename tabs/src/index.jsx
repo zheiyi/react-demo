9adeff48510eaef7b1c7dcd7a8beb9012d5e7a4d
y00418265 <yuanxiaozheng@huawei.com>
2018-03-19 20:03:21 +0800

    tabs

--- tabs/src/index.jsx
+++ tabs/src/index.jsx
@@ -1,12 +1,27 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+// import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
 
-import Hello from '../component/hello.jsx';
-import './index.scss';
+// import Hello from '../component/hello.jsx';
+// import './index.scss';
 
-ReactDOM.render(
-    <div className="page">
-        <Hello/>
-    </div>,
-    document.getElementById('page')
-);
+// ReactDOM.render(
+//     <div className="page">
+//         <Hello/>
+//     </div>,
+//     document.getElementById('page')
+// );
+
+import Tabs from '../component/tabs.jsx';
+import '../component/tabs.scss';
+
+const tab = new Tabs({
+    element: '#tab-demo',
+    tabs: '#tab-demo .tabs-nav li',
+    panels: '#tab-demo .tabs-content div',
+    activeIndex: 1,
+});
+
+tab.events.on('change', (o) => {
+    console.log(o);
+});
+
--- tabs/webpack.config.js
+++ tabs/webpack.config.js
@@ -14,9 +14,9 @@ module.exports = {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader',
-                query: {
-                    presets: ['es2015','react']
-                }
+                // query: {
+                //     presets: ['es2015','react']
+                // }
             },
             {
                 test: /\.(png|jpg|gif)$/,
